526784ba8499b43808e4354438c8d0ce9a3aa3d2
Søren Døssing <netcom@sauber.net>
2026-03-06 10:24:10 +0100

    Get instances of all bots

--- play.ts
+++ play.ts
@@ -1,16 +1,8 @@
 import type { Chess } from "chess.js";
 import { gameDashboard } from "./src/game-dashboard.ts";
-import { Random } from "./src/bot/random.ts";
 import { type Callback, ChessGame } from "./src/game.ts";
 import type { Player } from "./src/player.ts";
-import { First } from "./src/bot/first.ts";
-import { Last } from "./src/bot/last.ts";
-import { Hunt } from "./src/bot/hunt.ts";
-import { Flee } from "./src/bot/flee.ts";
-import { Middle } from "./src/bot/middle.ts";
-import { Capture } from "./src/bot/capture.ts";
-import { Center } from "./src/bot/center.ts";
-import { Edge } from "./src/bot/edge.ts";
+import { bots } from "./src/bot/mod.ts";
 
 // Dashboard callback
 let height: number;
@@ -25,17 +17,7 @@ const afterMove: Callback = (game: Chess, white: Player, black: Player) => {
 // const white: Player = new Flee();
 // const black: Player = new Hunt();
 
-const allPlayers: Player[] = [
-  new Random(),
-  new First(),
-  new Middle(),
-  new Last(),
-  new Hunt(),
-  new Flee(),
-  new Capture(),
-  new Center(),
-  new Edge(),
-];
+const allPlayers: Player[] = bots();
 const [black, white] = allPlayers.sort(() => Math.random() - 0.5);
 
 const game = new ChessGame(white, black, { afterMove });
---
+++ src/bot/mod.ts
@@ -0,0 +1,32 @@
+import type { Player } from "../player.ts";
+
+import { Capture } from "./capture.ts";
+export { Capture } from "./capture.ts";
+import { Center } from "./center.ts";
+export { Center } from "./center.ts";
+import { Edge } from "./edge.ts";
+export { Edge } from "./edge.ts";
+import { First } from "./first.ts";
+export { First } from "./first.ts";
+import { Flee } from "./flee.ts";
+export { Flee } from "./flee.ts";
+import { Hunt } from "./hunt.ts";
+export { Hunt } from "./hunt.ts";
+import { Last } from "./last.ts";
+export { Last } from "./last.ts";
+import { Middle } from "./middle.ts";
+export { Middle } from "./middle.ts";
+import { Random } from "./random.ts";
+export { Random } from "./random.ts";
+
+export const bots = (): Player[] => [
+  new Capture(),
+  new Center(),
+  new Edge(),
+  new First(),
+  new Flee(),
+  new Hunt(),
+  new Last(),
+  new Middle(),
+  new Random(),
+];
