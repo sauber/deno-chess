461e0e99a737fad58fb641e63195432afd9593fd
Søren Døssing <netcom@sauber.net>
2026-03-09 09:29:21 +0100

    Display PGN string after game

--- play.ts
+++ play.ts
@@ -8,8 +8,8 @@ import { bots } from "./src/bot/mod.ts";
 let height: number;
 const afterMove: Callback = (game: Chess, white: Player, black: Player) => {
   const output: string = gameDashboard(game, white, black);
-  // const cursorUp: string = height > 0 ? "\x1b[" + height + "A" : "";
-  const cursorUp: string = "\n";
+  const cursorUp: string = height > 0 ? "\x1b[" + height + "A" : "";
+  // const cursorUp: string = "\n";
   console.log(cursorUp + output);
   height = output.split("\n").length;
 };
@@ -19,3 +19,4 @@ const [black, white] = allPlayers.sort(() => Math.random() - 0.5);
 
 const game = new ChessGame(white, black, { afterMove });
 game.play();
+console.log(game.chess.pgn());
