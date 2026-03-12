5f742bf4d8834c2189deade93562266e61bbbab7
Søren Døssing <netcom@sauber.net>
2026-03-12 16:47:40 +0100

    Speed up Score bot

--- src/bot/score.ts
+++ src/bot/score.ts
@@ -1,57 +1,44 @@
-import {
-  BLACK,
-  type Chess,
-  type Color,
-  KING,
-  type Move,
-  type PieceSymbol,
-  WHITE,
-} from "chess.js";
+import { BLACK, type Chess, type Color, type Move, WHITE } from "chess.js";
 import { Player } from "../player.ts";
 import {
   indexToName,
+  type Indices,
   pieces,
   pieceValue,
   type SquareIndex,
 } from "./helpers.ts";
-import type { Moves } from "../types.ts";
 
 /** Value of piece on square */
 const squareValue = (board: Chess, square: SquareIndex): number => {
-  const piece: PieceSymbol = board.get(indexToName(square))
-    ?.type as PieceSymbol;
-  if (piece === KING) return 1;
-  return pieceValue[piece];
+  const piece = board.get(indexToName(square));
+  return piece ? pieceValue[piece.type] : 0;
 };
 
 /** Sum of values of all pieces belonging to player, except king */
-const boardValue = (board: Chess, color: Color): number =>
-  pieces(color, board).map((square: SquareIndex) => squareValue(board, square))
+const boardValue = (board: Chess, playerPieces: Indices): number =>
+  playerPieces.map((square: SquareIndex) => squareValue(board, square))
     .reduce((a, b) => a + b, 0) - 1000;
 
 /** Sum of values of all opponent pieces that are attacked */
-const attackValue = (board: Chess, color: Color): number =>
-  pieces(color === WHITE ? BLACK : WHITE, board).map((
+const attackValue = (
+  board: Chess,
+  attackerColor: Color,
+  attackedPieces: Indices,
+): number =>
+  attackedPieces.map((
     square: SquareIndex,
   ) =>
-    board.attackers(indexToName(square), color).length > 0
+    board.attackers(indexToName(square), attackerColor).length > 0
       ? squareValue(board, square)
       : 0
-  ).reduce((a, b) => a + b, 0);
-
-/** Count of possible moves */
-const mobilityValue = (board: Chess, color: Color): number =>
-  pieces(color, board).map((square: SquareIndex) =>
-    board.moves({ square: indexToName(square) }).length
   ).reduce((a, b) => a + b, 0);
 
 /** Calculate board score of move */
 export class Score extends Player {
   name = "Score";
   rank = (move: Move, game: Chess): number => {
-    const [player, opponent]: [Color, Color] = game.turn() === WHITE
-      ? [WHITE, BLACK]
-      : [BLACK, WHITE];
+    const player: Color = game.turn();
+    const opponent: Color = player === WHITE ? BLACK : WHITE;
 
     // Capture opponent
     let score = move.captured ? pieceValue[move.captured] / 2 : 0;
@@ -60,12 +47,16 @@ export class Score extends Player {
     score += move.promotion ? pieceValue[move.promotion] : 0;
 
     game.move(move);
-    score += boardValue(game, player) +
-      attackValue(game, player) +
-      0.1 * mobilityValue(game, player) -
-      boardValue(game, opponent) -
-      attackValue(game, opponent) -
-      0.1 * mobilityValue(game, opponent);
+
+    const playerPieces = pieces(player, game);
+    const opponentPieces = pieces(opponent, game);
+
+    score += boardValue(game, playerPieces) +
+      attackValue(game, player, opponentPieces) -
+      boardValue(game, opponentPieces) -
+      attackValue(game, opponent, playerPieces) -
+      0.1 * game.moves().length; // Opponent mobility
+
     game.undo();
     return score;
   };
